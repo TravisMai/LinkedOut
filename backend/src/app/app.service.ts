6f517709a38fe37636bf2b84e2e6ce3f391e2c9f
Mai Hữu Nghĩa <93875063+TravisMai@users.noreply.github.com>
2024-04-29 12:49:41 +0700

    Update app.service.ts

--- backend/src/app/app.service.ts
+++ backend/src/app/app.service.ts
@@ -1,19 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { CompanyRepository } from 'src/module/company/company.repository';
-import { InternshipRepository } from 'src/module/internship/internship.repository';
 import { JobRepository } from 'src/module/job/job.repository';
-import { JobApplicantsRepository } from 'src/module/jobApplicants/jobApplicants.repository';
-import { StaffRepository } from 'src/module/staff/staff.repository';
 import { StudentRepository } from 'src/module/student/student.repository';
 
 @Injectable()
 export class AppService {
   constructor(
     private studentRepo: StudentRepository,
-    private internshipRepo: InternshipRepository,
     private jobRepo: JobRepository,
-    private jobApplicantsRepo: JobApplicantsRepository,
-    private staffRepo: StaffRepository,
     private companyRepo: CompanyRepository,
   ) {}
 
@@ -25,7 +19,6 @@ export class AppService {
     const queryResults = await Promise.all([
       this.getCompanies(normalizedKeyword),
       this.getJobs(normalizedKeyword),
-      this.getStaff(normalizedKeyword),
       this.getStudents(normalizedKeyword),
     ]);
 
@@ -40,7 +33,7 @@ export class AppService {
     return this.companyRepo
       .createQueryBuilder('company')
       .select([
-        `'company' AS source_table`,
+        `'company' AS entity`,
         'company.id AS id',
         'company.name AS original_search_field',
         'company.created AS created',
@@ -56,7 +49,7 @@ export class AppService {
     return this.jobRepo
       .createQueryBuilder('job')
       .select([
-        `'job' AS source_table`,
+        `'job' AS entity`,
         'job.id AS id',
         'job.title AS original_search_field',
         'job.created AS created',
@@ -66,25 +59,11 @@ export class AppService {
       .getRawMany();
   }
 
-  private getStaff(keyword: string) {
-    return this.staffRepo
-      .createQueryBuilder('staff')
-      .select([
-        `'staff' AS source_table`,
-        'staff.id AS id',
-        'staff.name AS original_search_field',
-        'staff.created AS created',
-        'staff.updated AS updated',
-      ])
-      .where('unaccent(staff.name) ILIKE :keyword', { keyword: `%${keyword}%` })
-      .getRawMany();
-  }
-
   private getStudents(keyword: string) {
     return this.studentRepo
       .createQueryBuilder('student')
       .select([
-        `'student' AS source_table`,
+        `'student' AS entity`,
         'student.id AS id',
         'student.name AS original_search_field',
         'student.created AS created',
