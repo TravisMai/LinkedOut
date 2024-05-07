d7f24328b63575cb9f4378f2dcd4c46f653c82e4
Mai Hữu Nghĩa <93875063+TravisMai@users.noreply.github.com>
2024-05-07 15:28:39 +0700

    BE: DatabaseInitializer

---
+++ backend/src/common/service/databaseInitialize.service.ts
@@ -0,0 +1,14 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { DataSource } from 'typeorm';
+
+@Injectable()
+export class DatabaseInitializerService implements OnModuleInit {
+  constructor(private dataSource: DataSource) {}
+
+  async onModuleInit() {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.query("CREATE EXTENSION IF NOT EXISTS unaccent");
+    await queryRunner.release();
+  }
+}
