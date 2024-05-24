6dd756ae124a514e5553960a49d38064fc477422
TranTriDat2002 <trantridat2002@gmail.com>
2024-05-25 02:50:18 +0700

    backend: get internship by student Id now also show company info

--- backend/src/module/internship/internship.repository.ts
+++ backend/src/module/internship/internship.repository.ts
@@ -25,6 +25,7 @@ export class InternshipRepository extends Repository<Internship> {
     return await this.createQueryBuilder('internship')
       .leftJoinAndSelect('internship.jobApplicants', 'jobApplicants')
       .leftJoinAndSelect('jobApplicants.job', 'job')
+      .leftJoinAndSelect('job.company', 'company')
       .leftJoinAndSelect('jobApplicants.student', 'student')
       .where('student.id = :studentId', { studentId })
       .getMany();
